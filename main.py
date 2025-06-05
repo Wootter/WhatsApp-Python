c245c8c26913019bcfa59ad5007ee3b8577566d6
Wout <woudeelen@gmail.com>
2025-06-05 11:25:02 +0200

    Added main code main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ Github repo: https://github.com/Wootter/WhatsApp-Python
 Funtion: A python communications method for WhatsApp
 How to use:
 """
+
+from twilio.rest import Client
+
+client = Client()
+
+from_whatsapp_number = 'whatsapp:+14155238886'  # Twilio sandbox number
+to_whatsapp_number = 'whatsapp:' + os.environ['TO_WHATSAPP_NUMBER']  # Your WhatsApp number
+
+client.messages.create(body='Hello World!',
+                       from_=from_whatsapp_number,
+                       to=to_whatsapp_number)
