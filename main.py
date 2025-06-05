fb8a11e655605edd20894c800ab0c5020777c98d
Wout <woudeelen@gmail.com>
2025-06-05 11:20:13 +0200

    Added main.py with header

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Name: WhatsApp Python
+Author: Wootter
+Date: 6th of June 2025
+Github repo: https://github.com/Wootter/WhatsApp-Python
+Funtion: A python communications method for WhatsApp
+How to use:
+"""
